fbe1c88cf4b7c2bdd9ea9376764e4490138d78fa
Haydee <haydee.hernandez@cimat.mx>
2025-05-20 13:24:02 -0600

    Actualizo contenido de scope.rst y references.rst

--- docs/conf.py
+++ docs/conf.py
@@ -1,2 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 project = 'Testing Guide STLC'
+html_theme = "furo"
+
+html_logo = "_static/logo.png"
+html_favicon = "_static/favicon.ico"
+html_static_path = ['_static']
